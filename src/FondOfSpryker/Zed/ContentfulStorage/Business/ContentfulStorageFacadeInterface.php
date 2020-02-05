4c76b6864efd427b331390c1c69160355e1dff4a
Julian Zimmermann <julian.zimmermann@fondof.de>
2020-02-05 14:03:08 +0100

    spryker update

--- src/FondOfSpryker/Zed/ContentfulStorage/Business/ContentfulStorageFacadeInterface.php
+++ src/FondOfSpryker/Zed/ContentfulStorage/Business/ContentfulStorageFacadeInterface.php
@@ -16,29 +16,5 @@ interface ContentfulStorageFacadeInterface
      *
      * @return void
      */
-    public function update(array $contentfulEntryIds): void;
-
-    /**
-     * @param array $contentfulEntryIds
-     *
-     * @return void
-     */
     public function unpublish(array $contentfulEntryIds): void;
-
-    /**
-     * @return int
-     */
-    public function importLastChangedEntries(): int;
-
-    /**
-     * @return int
-     */
-    public function importAllEntries(): int;
-
-    /**
-     * @param string $entryId
-     *
-     * @return int
-     */
-    public function importEntry(string $entryId): int;
 }
